cba7175c5f72f1059a0ca475dca04ee380bcf6e8
Aaron Molina <2aaronmolina@gmail.com>
2020-04-15 19:16:19 -0400

    use of sass and html updates

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,8 +1,9 @@
 window.addEventListener(
   "load",
   function () {
-    const websiteId = 255731256;
+    // github API logic
     (async function () {
+      const websiteId = 255731256;
       const repos = await fetch("https://api.github.com/users/aaronm-git/repos").then((res) => res.json());
       console.log(repos);
       const panelLinksContainer = document.getElementById("panel-links-container");
@@ -15,12 +16,53 @@ window.addEventListener(
         panelLinkHTML.querySelector("span.repoName").innerText = repo.name;
         if (websiteId === repo.id) {
           const tagHTML = stringToHTML(
-            `<div class="tags" style="margin-left: 1rem;"><span class="tag is-primary">This Website</span></div>`
+            `<div class="tags" style="margin-left: 1rem;"><span class="tag is-success is-light heading">this Website</span></div>`
           );
           panelLinkHTML.append(tagHTML);
         }
         panelLinksContainer.append(panelLinkHTML);
       });
+    })();
+
+    // navbar burger logic
+    (function () {
+      document.addEventListener("DOMContentLoaded", () => {
+        // Get all "navbar-burger" elements
+        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
+
+        // Check if there are any navbar burgers
+        if ($navbarBurgers.length > 0) {
+          // Add a click event on each of them
+          $navbarBurgers.forEach((el) => {
+            el.addEventListener("click", () => {
+              // Get the target from the "data-target" attribute
+              const target = el.dataset.target;
+              const $target = document.getElementById(target);
+
+              // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+              el.classList.toggle("is-active");
+              $target.classList.toggle("is-active");
+            });
+          });
+        }
+      });
+    })();
+
+    // anchor smooth scroll
+    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+      anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+          behavior: "smooth",
+        });
+      });
+    });
+
+    // tilt.js init
+    (function () {
+      const element = document.querySelector(".js-tilt");
+      VanillaTilt.init(element);
     })();
   },
   { once: true }
@@ -40,3 +82,16 @@ function getPanelLinkHTML() {
       <span class="repoName"></span>
     </a>`;
 }
+
+window.addEventListener("scroll", function () {
+  const el = document.getElementById("top-botton");
+  if (window.scrollY > 600) {
+    if (el.classList.contains("is-hidden")) {
+      el.classList.remove("is-hidden");
+    }
+  } else {
+    if (!el.classList.contains("is-hidden")) {
+      el.classList.add("is-hidden");
+    }
+  }
+});
