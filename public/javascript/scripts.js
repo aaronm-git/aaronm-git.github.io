4ecd60d38a82bdd6b1ab3c758c8595ebfd0c0989
Aaron Molina <2aaronmolina@gmail.com>
2020-04-16 14:02:25 -0400

    updated scripts

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,73 +1,81 @@
-window.addEventListener(
-  "load",
-  function () {
-    // github API logic
-    (async function () {
-      const websiteId = 255731256;
-      const repos = await fetch("https://api.github.com/users/aaronm-git/repos").then((res) => res.json());
-      console.log(repos);
-      const panelLinksContainer = document.getElementById("panel-links-container");
-      repos.sort((a, b) => (new Date(a.updated_at) < new Date(b.updated_at) ? 1 : -1));
-      repos.forEach((repo) => {
-        const panelLinkHTML = stringToHTML(getPanelLinkHTML());
-        panelLinkHTML.setAttribute("href", repo.html_url);
-        let iClassList = panelLinkHTML.querySelector("i").classList;
-        repo.fork ? iClassList.add("fas", "fa-code-branch") : iClassList.add("fas", "fa-book");
-        panelLinkHTML.querySelector("span.repoName").innerText = repo.name;
-        if (websiteId === repo.id) {
-          const tagHTML = stringToHTML(
-            `<div class="tags" style="margin-left: 1rem;"><span class="tag is-success is-light heading">this Website</span></div>`
-          );
-          panelLinkHTML.append(tagHTML);
-        }
-        panelLinksContainer.append(panelLinkHTML);
-      });
-    })();
-
-    // navbar burger logic
-    (function () {
-      document.addEventListener("DOMContentLoaded", () => {
-        // Get all "navbar-burger" elements
-        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
-
-        // Check if there are any navbar burgers
-        if ($navbarBurgers.length > 0) {
-          // Add a click event on each of them
-          $navbarBurgers.forEach((el) => {
-            el.addEventListener("click", () => {
-              // Get the target from the "data-target" attribute
-              const target = el.dataset.target;
-              const $target = document.getElementById(target);
-
-              // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-              el.classList.toggle("is-active");
-              $target.classList.toggle("is-active");
-            });
-          });
-        }
-      });
-    })();
-
-    // anchor smooth scroll
-    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-      anchor.addEventListener("click", function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-          behavior: "smooth",
+// nav-burger logic
+{
+  document.addEventListener("DOMContentLoaded", () => {
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+      // Add a click event on each of them
+      $navbarBurgers.forEach((el) => {
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+        el.addEventListener("click", () => {
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle("is-active");
+          $target.classList.toggle("is-active");
         });
+        // close burger after clicking a link
+        $target.querySelectorAll(".navbar-start a").forEach((anchor) =>
+          anchor.addEventListener("click", (e) => {
+            el.classList.toggle("is-active");
+            $target.classList.toggle("is-active");
+          })
+        );
+      });
+    }
+  });
+}
+// to top button
+{
+  window.addEventListener("scroll", function () {
+    const el = document.getElementById("top-button");
+    if (window.scrollY > 600) {
+      if (el.classList.contains("is-hidden")) {
+        el.classList.remove("is-hidden");
+      }
+    } else {
+      if (!el.classList.contains("is-hidden")) {
+        el.classList.add("is-hidden");
+      }
+    }
+  });
+}
+// anchor smooth scroll
+{
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
       });
     });
+  });
+}
+// github API logic
+(async () => {
+  const websiteId = 255731256;
+  const repos = await fetch("https://api.github.com/users/aaronm-git/repos").then((res) => res.json());
+  const panelLinksContainer = document.getElementById("panel-links-container");
+  repos.sort((a, b) => (new Date(a.updated_at) < new Date(b.updated_at) ? 1 : -1));
+  repos.forEach((repo) => {
+    const panelLinkHTML = stringToHTML(getPanelLinkHTML());
+    panelLinkHTML.setAttribute("href", repo.html_url);
+    const iClassList = panelLinkHTML.querySelector("i").classList;
+    repo.fork ? iClassList.add("fas", "fa-code-branch") : iClassList.add("fas", "fa-book");
+    panelLinkHTML.querySelector("span.repoName").innerText = repo.name;
+    if (websiteId === repo.id) {
+      const tagHTML = stringToHTML(
+        `<div class="tags" style="margin-left: 1rem;"><span class="tag is-success is-light heading">this Website</span></div>`
+      );
+      panelLinkHTML.append(tagHTML);
+    }
+    panelLinksContainer.append(panelLinkHTML);
+  });
+})();
 
-    // tilt.js init
-    (function () {
-      const element = document.querySelector(".js-tilt");
-      VanillaTilt.init(element);
-    })();
-  },
-  { once: true }
-);
-
+// utils
 function stringToHTML(str) {
   var dom = document.createElement("div");
   dom.innerHTML = str;
@@ -82,16 +90,3 @@ function getPanelLinkHTML() {
       <span class="repoName"></span>
     </a>`;
 }
-
-window.addEventListener("scroll", function () {
-  const el = document.getElementById("top-botton");
-  if (window.scrollY > 600) {
-    if (el.classList.contains("is-hidden")) {
-      el.classList.remove("is-hidden");
-    }
-  } else {
-    if (!el.classList.contains("is-hidden")) {
-      el.classList.add("is-hidden");
-    }
-  }
-});
