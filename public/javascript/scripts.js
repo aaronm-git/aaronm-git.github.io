513515c60ed50190ace0989be592e565f22cc37f
Aaron Molina <2aaronmolina@gmail.com>
2020-04-27 15:25:28 -0400

    reenabled github api

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -71,26 +71,26 @@
   });
 }
 // github API logic
-// (async () => {
-//   const websiteId = 255731256;
-//   const repos = await fetch("https://api.github.com/users/aaronm-git/repos").then((res) => res.json());
-//   const panelLinksContainer = document.getElementById("panel-links-container");
-//   repos.sort((a, b) => (new Date(a.updated_at) < new Date(b.updated_at) ? 1 : -1));
-//   repos.forEach((repo) => {
-//     const panelLinkHTML = stringToHTML(getPanelLinkHTML());
-//     panelLinkHTML.setAttribute("href", repo.html_url);
-//     const iClassList = panelLinkHTML.querySelector("i").classList;
-//     repo.fork ? iClassList.add("fas", "fa-code-branch") : iClassList.add("fas", "fa-book");
-//     panelLinkHTML.querySelector("span.repoName").innerText = repo.name;
-//     if (websiteId === repo.id) {
-//       const tagHTML = stringToHTML(
-//         `<div class="tags" style="margin-left: 1rem;"><span class="tag is-primary is-light heading">this Website</span></div>`
-//       );
-//       panelLinkHTML.append(tagHTML);
-//     }
-//     panelLinksContainer.append(panelLinkHTML);
-//   });
-// })();
+(async () => {
+  const websiteId = 255731256;
+  const repos = await fetch("https://api.github.com/users/aaronm-git/repos").then((res) => res.json());
+  const panelLinksContainer = document.getElementById("panel-links-container");
+  repos.sort((a, b) => (new Date(a.updated_at) < new Date(b.updated_at) ? 1 : -1));
+  repos.forEach((repo) => {
+    const panelLinkHTML = stringToHTML(getPanelLinkHTML());
+    panelLinkHTML.setAttribute("href", repo.html_url);
+    const iClassList = panelLinkHTML.querySelector("i").classList;
+    repo.fork ? iClassList.add("fas", "fa-code-branch") : iClassList.add("fas", "fa-book");
+    panelLinkHTML.querySelector("span.repoName").innerText = repo.name;
+    if (websiteId === repo.id) {
+      const tagHTML = stringToHTML(
+        `<div class="tags" style="margin-left: 1rem;"><span class="tag is-primary is-light heading">this Website</span></div>`
+      );
+      panelLinkHTML.append(tagHTML);
+    }
+    panelLinksContainer.append(panelLinkHTML);
+  });
+})();
 
 // Skill Search Logic
 {
